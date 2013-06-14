5713ed59679dc369c54d377b8dbc492a598ab500
Sharon Lin <itsaviogiuseppe@gmail.com>
2013-06-14 19:00:14 +0800

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,6 +6,9 @@ describe "Authentication" do
     before { visit signin_path }
 	it { should have_selector('h1', text: 'Sign in') }
 	it { should have_selector('title', text: 'Sign in') }
+	
+	it { should_not have_link('Profile') }
+	it { should_not have_link('Settings') }
   end
   
   describe "signin" do
@@ -17,6 +20,9 @@ describe "Authentication" do
 	  it { should have_selector('title', text: 'Sign in') }
 	  #it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 	  it { should have_error_message('Invalid') }
+	  
+	  it { should_not have_link('Profile') }
+	  it { should_not have_link('Settings') }
 	  
 	  describe "after visiting another page" do
 	    before { click_link "Home" }
@@ -31,6 +37,7 @@ describe "Authentication" do
 	  #  fill_in "Password", with: user.password
 	  #  click_button "Sign in"
 	  #end
+	  
 	  before { valid_signin(user) }
 	  
 	  it { should have_selector('title', text: user.name) }
@@ -49,13 +56,25 @@ describe "Authentication" do
 	  describe "when attempting to visit a protected page" do
 	    before do
 		  visit edit_user_path(user)
-		  fill_in "Email", with: user.email
-		  fill_in "Password", with: user.password
-		  click_button "Sign in"
+		#  fill_in "Email", with: user.email
+		#  fill_in "Password", with: user.password
+		#  click_button "Sign in"
+		  sign_in user
 		end
-		describe "after in" do
+		describe "after signing in" do
 		  it "should render the desired protected page" do
 		    page.should have_selector('title', text: 'Edit user')
+		  end
+		  
+		  describe "when signing in again" do
+		    before do
+			  delete signout_path
+			  visit signin_path
+			  sign_in user
+			end
+			it "should render the default (profile) page" do
+			  page.should have_selector('title', text: user.name)
+			end
 		  end
 		end
 	  end
@@ -72,6 +91,18 @@ describe "Authentication" do
 		describe "visiting the user index" do
 		  before { visit users_path }
 		  it { should have_selector('title', text: 'Sign in') }
+		end
+	  end
+	  
+	  describe "in the Microposts controller" do
+	    describe "submitting to the create action" do
+		  before { post microposts_path }
+		  specify { response.should redirect_to(signin_path) }
+		end
+		
+		describe "submitting to the destroy action" do
+		  before { delete micropost_path(FactoryGirl.create(:micropost)) }
+		  specify { response.should redirect_to(signin_path) }
 		end
 	  end
 	end
@@ -101,6 +132,10 @@ describe "Authentication" do
 	    before { delete user_path(user) }
 		specify { response.should redirect_to(root_path) }
 	  end
+	end
+	
+	describe "as an admin user" do
+	  
 	end
   end
 end
